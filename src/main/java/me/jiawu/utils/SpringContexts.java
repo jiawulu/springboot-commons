09bc3483d029335278d41d157d290b773cc1f1f3
lujiawu <jiawu.lu@gmail.com>
2017-12-25 08:25:49 +0800

    [feat] add spring context & unit test

---
+++ src/main/java/me/jiawu/utils/SpringContexts.java
@@ -0,0 +1,30 @@
+package me.jiawu.utils;
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author wuzhong on 2017/12/25.
+ * @version 1.0
+ */
+@Component
+public class SpringContexts implements ApplicationContextAware {
+
+    public static ApplicationContext context;
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        SpringContexts.context = applicationContext;
+    }
+
+    public static <T> T getBean(Class<T> clazz) {
+        return context.getBean(clazz);
+    }
+
+    public static <T> T getBean(String name, Class<T> clazz) {
+        return context.getBean(name, clazz);
+    }
+
+}
